960d80a1aef120322a8cd2005e84b6e4357bb2c9
Js | React | Next | TypeScript | Node | Express | MernStack <59826852+mahabur-rahman@users.noreply.github.com>
2022-06-04 10:43:38 +0600

    password generator

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>this is app</h1>
+    </>
+  );
+};
+
+export default App;
